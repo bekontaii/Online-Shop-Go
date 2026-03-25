1b182fed783c143e91403d3c6ef26128d3334cd0
bekontaii <241870@astanait.edu.kz>
2026-03-25 23:01:19 +0500

    product side

--- internal/product/model.go
+++ internal/product/model.go
@@ -1,7 +1,12 @@
 package product
 
+import "time"
+
 type Product struct {
+	ID          int
 	Name        string
 	Price       float64
 	Description string
+	CreatedDate time.Time
+	UpdatedDate time.Time
 }
